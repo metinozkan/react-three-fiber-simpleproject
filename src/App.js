6f5b231a16d8f0e651c2601c14fc922941f5044f
metinozkan <metin.ozkn@hotmail.com>
2021-09-29 18:11:32 +0300

    added physics with use-cannon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import { Canvas } from "@react-three/fiber";
 import { Stars, OrbitControls } from "@react-three/drei";
+
+import { Physics, usePlane, useBox } from "@react-three/cannon";
 const Box = () => {
+  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 3, 0] }));
   return (
-    <mesh>
+    <mesh
+      ref={ref}
+      onClick={() => {
+        api.velocity.set(2, 2, 0);
+      }}
+    >
       <boxBufferGeometry attach="geometry" />
       <meshLambertMaterial attach="material" color="red" />
+    </mesh>
+  );
+};
+
+const Plane = () => {
+  const [ref, api] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+    args: [5, 5],
+  }));
+  return (
+    <mesh ref={ref}>
+      <planeBufferGeometry attach="geometry" args={[20, 20]} />
+      <meshLambertMaterial attach="material" color="brown" />
     </mesh>
   );
 };
@@ -14,7 +35,11 @@ function App() {
     <Canvas>
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 15, 10]} angle={0.3} />
-      <Box />
+      <Physics>
+        <Box />
+        <Plane />
+      </Physics>
+
       <Stars />
       <OrbitControls />
     </Canvas>
